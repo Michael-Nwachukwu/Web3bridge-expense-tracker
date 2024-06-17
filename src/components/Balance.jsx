2bed54b3e82a211c5723d0c55fa3a0f8e7dcbbb1
Michael Nwachukwu <dahformulah@gmail.com>
2024-06-17 14:59:56 +0100

    readme

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -1,15 +1,13 @@
-import React from 'react'
+import React from 'react']
+import TransactionForm from './TransactionForm'
 
 const Balance = () => {
     return (
         <div className='flex flex-col items-start gap-3'>
             <h1 className="text-3xl font-bold text-indigo-700">Wallet Balance</h1>
             <p className="text-xl text-gray-600">$1000</p>
-            <div className="inline-flex items0center gap-6">
-                <button className="bg-indigo-500 text-white px-2 py-1 rounded-xl">Add income</button>
-                <button className="bg-indigo-500 text-white px-2 py-1 rounded-xl">Add expense</button>
-            </div>
-
+        <TransactionForm addTransaction={addTransaction} />
+            
         </div>
     )
 }
